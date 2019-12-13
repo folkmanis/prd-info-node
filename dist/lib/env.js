4e478254fef58d668d41240ea46eee9e43fffd01
Developer <dev@dev.local>
2019-12-13 12:00:50 +0000

    Moved dist folder

--- dist/lib/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-dotenv_1.config();
-//# sourceMappingURL=env.js.map
--- dist/lib/sessionDAO.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=sessionDAO.js.map
