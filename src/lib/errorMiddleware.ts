94db733c5363d97eef4b94b1a6338f899ce4e3e8
Developer <dev@dev.local>
2020-04-01 13:05:52 +0000

    Error middleware added

---
+++ src/lib/errorMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function logError(error: Error, req: Request, res: Response, next: NextFunction) {
+    // tslint:disable-next-line:no-console
+    // console.log('Error occured in the controller class ', error.message);
+    req.log.error('Error occured', error.message)
+    // tslint:disable-next-line:no-console
+    // console.log('Stopping request processing');
+    res.status(404).json({ error: error.message });
+}
+
+export function logErrorAndContinue(error: Error, req: Request, res: Response, next: NextFunction) {
+    // tslint:disable-next-line:no-console
+    console.log('Error occured in the request', error.message);
+    // tslint:disable-next-line:no-console
+    console.log('Continuing with request processing');
+    next(error);
+}
