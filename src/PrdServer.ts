549924fba90ed0c742d549c323050cd6324b6849
Developer <dev@dev.local>
2020-07-26 20:01:43 +0000

    set 'trust-proxy'

--- src/PrdServer.ts
+++ src/PrdServer.ts
@@ -15,7 +15,8 @@ export class PrdServer extends Server {
         super(true);
         Logger.addTransport(new Console());  // Pievieno konsoles izvadi Logger objektam
         this.app.use(Logger.handler); // Logger funkcijas būs pieejamas kā req.log
-        this.app.use(bodyParser.json({limit: process.env.BODY_SIZE_LIMIT}));
+        this.app.set('trust proxy', true);
+        this.app.use(bodyParser.json({ limit: process.env.BODY_SIZE_LIMIT }));
         this.app.use(bodyParser.urlencoded({ extended: true }));
     }
 
