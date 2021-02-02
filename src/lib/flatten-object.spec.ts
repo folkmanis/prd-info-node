9fa9b115d1fe81482e11c8765d14a791683bfc80
Edmunds Folkmanis <folkmanis@gmail.com>
2021-02-02 11:31:05 +0000

    flatten object accepts deepth

---
+++ src/lib/flatten-object.spec.ts
@@ -0,0 +1,61 @@
+import { flattenObject } from './flatten-object';
+
+const obj = {
+    "settings": {
+        "productCategories": [
+            {
+                "category": "plates",
+                "description": "Iespiedforma"
+            },
+        ],
+        "lastJobId": 5001,
+    }
+};
+
+const resultOneLevel = {
+    "settings.productCategories": [
+        {
+            "category": "plates",
+            "description": "Iespiedforma"
+        },
+    ],
+    "settings.lastJobId": 5001,
+};
+
+const resultTwoLevel = {
+    "settings.productCategories.0": {
+        "category": "plates",
+        "description": "Iespiedforma"
+    },
+    "settings.lastJobId": 5001,
+
+};
+
+const resultFull = {
+    "settings.productCategories.0.category": "plates",
+    "settings.productCategories.0.description": "Iespiedforma",
+    "settings.lastJobId": 5001,
+};
+
+describe('Should flatten object n deep', () => {
+    test('flatten one level', () =>
+        expect(flattenObject(obj, 1)).toEqual(resultOneLevel)
+    );
+    test('flatten two level', () =>
+        expect(flattenObject(obj, 2)).toEqual(resultTwoLevel)
+    );
+    test('flatten full deep without second argument', () => {
+        expect(flattenObject(obj)).toEqual(resultFull);
+    });
+    test('Should throw error when deep is not integer >0', () => {
+        expect(() => flattenObject(obj, -1)).toThrow('maxDepth should be positive integer value');
+    });
+    test('Should throw error when no object provided', () => {
+        expect(() => flattenObject(obj, 'string' as unknown as number)).toThrow();
+        expect(() => flattenObject(obj, null as unknown as number)).toThrow();
+        expect(() => flattenObject(obj, (() => { }) as unknown as number)).toThrow();
+    });
+    test('Should return empty object from empty object', () => {
+        expect(flattenObject({})).toEqual({});
+    });
+});
--- src/lib/flatten-object.ts
+++ src/lib/flatten-object.ts
@@ -1,16 +1,15 @@
 
-export function flattenObject(ob: {[key: string]: any}) {
-	const toReturn: {[key: string]: any} = {};
-	
-	for (const i in ob) {
-		if (!ob.hasOwnProperty(i)) continue;
-		
-		if ((typeof ob[i]) == 'object') {
-			var flatObject = flattenObject(ob[i]);
-			for (var x in flatObject) {
-				if (!flatObject.hasOwnProperty(x)) continue;
-				
-				toReturn[i + '.' + x] = flatObject[x];
+export function flattenObject(ob: { [key: string]: any; }, maxDepth = Infinity): { [key: string]: any; } {
+	if (maxDepth < 0 || Math.floor(maxDepth) !== maxDepth) {
+		throw new Error('maxDepth should be positive integer value');
+	}
+	const toReturn: { [key: string]: any; } = {};
+	for (const i of Object.keys(ob)) {
+
+		if (typeof ob[i] === 'object' && maxDepth > 0) {
+			const flatObject = flattenObject(ob[i], maxDepth - 1);
+			for (const k of Object.keys(flatObject)) {
+				toReturn[i + '.' + k] = flatObject[k];
 			}
 		} else {
 			toReturn[i] = ob[i];
