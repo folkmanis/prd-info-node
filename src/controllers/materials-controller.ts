da642b80c012dcf8998262ad9b49acf59a19b623
Edmunds Folkmanis <folkmanis@gmail.com>
2021-07-07 07:27:41 +0000

    added basic api functions

--- src/controllers/materials-controller.ts
+++ src/controllers/materials-controller.ts
@@ -5,17 +5,77 @@ import { asyncWrapper } from '../lib/asyncWrapper';
 import { logError } from '../lib/errorMiddleware';
 import { PrdSession } from '../lib/session-handler';
 import { MaterialsDao } from '../dao';
+import { Material } from '../interfaces/materials.interface';
 
 @Controller('data/materials')
 @ClassErrorMiddleware(logError)
 @ClassMiddleware([
+    Preferences.getUserPreferences,
     PrdSession.validateSession,
+    PrdSession.validateModule('calculations'),
 ])
 @ClassWrapper(asyncWrapper)
 export class MaterialsController {
 
     constructor(
-        materialsDao: MaterialsDao,
+        private materialsDao: MaterialsDao,
     ) { }
+
+    @Put()
+    async putMaterial(req: Request, res: Response) {
+        const mat = req.body as Material;
+        res.json({
+            error: false,
+            data: await this.materialsDao.addMaterial(mat)
+        });
+    }
+
+    @Post(':id')
+    async postMaterial(req: Request, res: Response) {
+        const id: string = req.params.id;
+        const mat = req.body as Partial<Material>;
+        delete mat._id;
+        const { modifiedCount } = await this.materialsDao.updateMaterial(id, mat);
+        res.json({
+            error: false,
+            modifiedCount,
+        });
+    }
+
+    @Delete(':id')
+    async deleteMaterial(req: Request, res: Response) {
+        const id: string = req.params.id;
+        const { deletedCount } = await this.materialsDao.deleteMaterial(id);
+        res.json({
+            error: false,
+            deletedCount,
+        });
+    }
+
+    @Get('validate/:property')
+    async getProperty(req: Request, res: Response) {
+        const property = req.params.property as keyof Material;
+        res.json({
+            error: false,
+            validatorData: await this.materialsDao.validationData(property),
+        });
+    }
+
+    @Get(':id') // (^[0-9a-fA-F]{24}$)
+    async getMaterial(req: Request, res: Response) {
+        const id: string = req.params.id;
+        res.json({
+            error: false,
+            data: await this.materialsDao.getMaterialById(id),
+        });
+    }
+
+    @Get('')
+    async getMaterials(req: Request, res: Response) {
+        res.json({
+            error: false,
+            data: await this.materialsDao.getMaterials(),
+        });
+    }
 
 }
--- src/dao/materialsDAO.ts
+++ src/dao/materialsDAO.ts
@@ -1,4 +1,4 @@
-import { MongoClient, Collection, Db } from 'mongodb';
+import { Collection, Db, FilterQuery, ObjectId, UpdateWriteOpResult, DeleteWriteOpResultObject } from 'mongodb';
 import Logger from '../lib/logger';
 import { Material } from '../interfaces/materials.interface';
 import { Dao } from '../interfaces/dao.interface';
@@ -17,6 +17,55 @@ export class MaterialsDao extends Dao {
             Logger.error('Materials DAO', err.message);
             return;
         }
+        this.createIndexes();
     }
+
+    getMaterials(filter: FilterQuery<Material> = {}): Promise<Partial<Material[]>> {
+        return this.materials
+            .find(filter)
+            .sort({ category: 1, name: 1 })
+            .toArray();
+    }
+
+    getMaterialById(id: string): Promise<Material | null> {
+        return this.materials.findOne(new ObjectId(id));
+    }
+
+    async addMaterial(mat: Material): Promise<ObjectId> {
+        const resp = await this.materials.insertOne(mat);
+        return resp.insertedId;
+    }
+
+    updateMaterial(id: string, mat: Partial<Material>): Promise<UpdateWriteOpResult> {
+        return this.materials.updateOne(
+            { _id: new ObjectId(id) },
+            {
+                $set: mat
+            }
+        );
+    }
+
+    deleteMaterial(id: string): Promise<DeleteWriteOpResultObject> {
+        return this.materials.deleteOne(
+            { _id: new ObjectId(id) }
+        );
+    }
+
+    async validationData<K extends keyof Material>(key: K): Promise<Array<Material[K]>> {
+        const resp = await this.materials.find()
+            .project({ _id: 0, [key]: 1 })
+            .toArray();
+        return resp.map(data => data[key]);
+    }
+
+    private createIndexes(): void {
+        this.materials.createIndexes([
+            {
+                key: { name: 1 },
+                unique: true,
+            }
+        ]);
+    }
+
 }
 
