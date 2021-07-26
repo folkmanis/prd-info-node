b1a4c52cc5f21f97a5ff7af9350073022d3459ca
Edmunds Folkmanis <folkmanis@gmail.com>
2021-07-26 13:44:22 +0000

    cleanup

--- src/dao/fileSystemDAO.ts
+++ src/dao/fileSystemDAO.ts
@@ -4,23 +4,24 @@ import path from 'path';
 import { Dao } from '../interfaces/dao.interface';
 import { Db } from 'mongodb';
 
-let rootPath: string;
 
 export class FileSystemDao extends Dao {
 
+    rootPath!: string;
+
     async injectDb(_: Db) {
-        rootPath = process.env.JOBS_INPUT as string;
+        this.rootPath = process.env.JOBS_INPUT as string;
     }
 
     async createFolder(folder: string[]): Promise<void> {
-        const fullPath = path.resolve(rootPath, ...folder);
+        const fullPath = path.resolve(this.rootPath, ...folder);
         const resp = await fsPromises.mkdir(fullPath, { recursive: true });
         Logger.info(`Folder ${fullPath} created`, { fullPath, resp });
         return;
     }
 
     resolveFullPath(folder: string[], filename: string): string {
-        return path.resolve(rootPath, ...folder, filename);
+        return path.resolve(this.rootPath, ...folder, filename);
     }
 
     writeFile(file: NodeJS.ReadableStream, folder: string[], filename: string) {
