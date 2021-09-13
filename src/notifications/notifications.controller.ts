be2028089d8de3d5819d9e31bf0603b9d482e75f
Edmunds Folkmanis <folkmanis@gmail.com>
2021-09-13 06:44:04 +0000

    added timestamp to response

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -7,24 +7,34 @@ import {
   Query,
 } from '@nestjs/common';
 import { NotificationsDaoService } from './notifications-dao/notifications-dao.service';
-import { NotificationModules } from './notification';
+import { NotificationModules, Notification } from './notification';
 import { InstanceId } from '../preferences/instance-id.decorator';
+
+interface NotificationResponse {
+  data: Notification[];
+  timestamp: Date;
+}
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private notificationsDao: NotificationsDaoService) {}
+  constructor(private notificationsDao: NotificationsDaoService) { }
 
   @Get()
   async getMessages(
     @Query('from', ParseIntPipe) fromDate: number,
     @Query('modules', ParseArrayPipe) modules: NotificationModules[],
     @InstanceId() instanceId: string,
-  ) {
-    return this.notificationsDao.getAll(
+  ): Promise<NotificationResponse> {
+    const toDate = new Date();
+    const data = await this.notificationsDao.getAll(
       new Date(fromDate),
-      new Date(),
+      toDate,
       modules,
       instanceId,
     );
+    return {
+      data,
+      timestamp: toDate,
+    };
   }
 }
