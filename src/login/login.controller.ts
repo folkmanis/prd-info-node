585af7d3374c94a09f83eb085c7fd8061fded67f
Edmunds Folkmanis <folkmanis@gmail.com>
2021-10-06 14:43:59 +0000

    cleanup

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Delete, Get, Post, Req, Session, UseGuards } from '@nestjs/common';
+import { UseInterceptors, Controller, Delete, Get, Post, Req, Session, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import * as session from 'express-session';
 import { User } from '../entities/users';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { PublicRoute } from './public-route.decorator';
 import { Usr } from '../session';
+import { ResponseWrapperInterceptor } from '../lib/response-wrapper.interceptor';
 
 @Controller('login')
 export class LoginController {
   @UseGuards(LocalAuthGuard)
   @PublicRoute()
   @Post()
-  async login(@Req() req: Request) {
+  async login(
+    @Req() req: Request
+  ) {
     req.session.user = req.user as User;
     req.session.lastSeen = {
       ip: req.ip,
@@ -22,15 +25,19 @@ export class LoginController {
 
   @Delete()
   @PublicRoute()
-  async logout(@Session() sess: session.Session) {
-    return new Promise((resolve) => sess.destroy(resolve)).then(
-      () => ({ response: 'logged out' })
-    );
+  @UseInterceptors(new ResponseWrapperInterceptor('response'))
+  async logout(
+    @Session() sess: session.Session
+  ) {
+    await new Promise(resolve => sess.destroy(resolve));
+    return 'logged out';
   }
 
   @Get()
   @PublicRoute()
-  user(@Usr() user: User | undefined) {
+  user(
+    @Usr() user: User | undefined
+  ) {
     return user || {};
   }
 }
