0945c832fd57dd5a49032c54ef0e6945d36a2553
Edmunds Folkmanis <folkmanis@gmail.com>
2021-11-05 14:20:28 +0000

    entity typed

--- src/entities/jobs/entities/job-one-product.ts
+++ src/entities/jobs/entities/job-one-product.ts
@@ -1,8 +1,15 @@
-import { OmitType, IntersectionType } from '@nestjs/mapped-types';
-import { Job } from './job.entity';
+import { OmitType } from '@nestjs/mapped-types';
+import { Type } from 'class-transformer';
+import { IsNumber, ValidateNested } from 'class-validator';
 import { JobProduct } from './job-product.entity';
+import { Job } from './job.entity';
 
 export class JobOneProduct extends OmitType(Job, ['products']) {
+
+    @Type(() => JobProduct)
+    @ValidateNested({ each: true })
     products: JobProduct;
+
+    @IsNumber()
     productsIdx: number;
 }
