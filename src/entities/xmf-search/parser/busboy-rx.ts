8c3efb6517857d6fd66aec8c9c5e1d28469c1338
Edmunds Folkmanis <folkmanis@gmail.com>
2022-02-18 19:37:17 +0000

    busboy update

--- src/entities/xmf-search/parser/busboy-rx.ts
+++ src/entities/xmf-search/parser/busboy-rx.ts
@@ -13,11 +13,11 @@ export function rxBusboy(req: Request): Observable<FileStream> {
     const busboy = Busboy({ headers: req.headers });
     busboy.on(
       'file',
-      (fieldname: string, file: NodeJS.ReadableStream, filename: string) =>
+      (fieldname: string, file: NodeJS.ReadableStream, fileInfo: Busboy.FileInfo) =>
         subscriber.next({
           fieldname,
           file,
-          filename,
+          filename: fileInfo.filename,
         }),
     );
 
