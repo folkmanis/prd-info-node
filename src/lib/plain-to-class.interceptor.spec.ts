e74f7ad013c5719068e09fab8468b5c83a8a67ce
Edmunds Folkmanis <folkmanis@gmail.com>
2022-03-04 12:51:35 +0000

    initial build

---
+++ src/lib/plain-to-class.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { PlainToClassInterceptor } from './plain-to-class.interceptor';
+
+describe('PlainToClassInterceptor', () => {
+  it('should be defined', () => {
+    expect(new PlainToClassInterceptor()).toBeDefined();
+  });
+});
