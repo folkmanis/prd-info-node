027e3b348b31229bc5569fef78422144252752a6
Edmunds Folkmanis <folkmanis@gmail.com>
2022-09-09 16:08:02 +0300

    cleanup

--- src/lib/pick-not-null.ts
+++ src/lib/pick-not-null.ts
@@ -1,15 +1,6 @@
 import { pickBy } from 'lodash';
 
-type NonNullType<
-  T extends Record<string | number, any>,
-  K extends keyof T = keyof T,
-> = { [key in NonNullable<K>]: T[key] };
 
-export function pickNotNull<T extends Record<string | number, any>>(
-  obj: T,
-): NonNullType<T> {
-  return pickBy(
-    obj,
-    (val) => val !== undefined && val !== null,
-  ) as NonNullType<T>;
+export function pickNotNull<T extends Record<string | number, any>>(obj: T): NonNullable<T> {
+  return pickBy<T>(obj, val => val !== undefined && val !== null) as NonNullable<T>;
 }
