e52a0788865cb09c49b42f226b17d7dd46106dd8
Edmunds Folkmanis <folkmanis@gmail.com>
2022-09-20 17:05:19 +0300

    products optional

--- src/entities/jobs/entities/job-one-product.ts
+++ src/entities/jobs/entities/job-one-product.ts
@@ -1,11 +1,12 @@
 import { OmitType } from '@nestjs/mapped-types';
 import { Type } from 'class-transformer';
-import { IsNumber, ValidateNested } from 'class-validator';
+import { IsNumber, IsOptional, ValidateNested } from 'class-validator';
 import { JobProduct } from './job-product.entity';
 import { Job } from './job.entity';
 
 export class JobOneProduct extends OmitType(Job, ['products']) {
   @Type(() => JobProduct)
+  @IsOptional()
   @ValidateNested({ each: true })
   products: JobProduct;
 
