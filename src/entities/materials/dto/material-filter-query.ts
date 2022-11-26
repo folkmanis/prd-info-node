b0d1c2f15f7cd50a64482fc3e6df0410882fb43f
Edmunds Folkmanis <folkmanis@gmail.com>
2022-11-26 18:33:24 +0200

    categories filter fix

--- src/entities/materials/dto/material-filter-query.ts
+++ src/entities/materials/dto/material-filter-query.ts
@@ -1,16 +1,16 @@
-import { Material } from '../entities/material.entity';
-import { StartLimitFilter } from '../../../lib/start-limit-filter/start-limit-filter.class';
-import { FilterType } from '../../../lib/start-limit-filter/filter-type.interface';
-import { IsString, IsOptional } from 'class-validator';
-import { deserializeArray, Transform } from 'class-transformer';
+import { Transform } from 'class-transformer';
+import { IsOptional, IsString } from 'class-validator';
 import { pickNotNull } from '../../../lib/pick-not-null';
+import { FilterType } from '../../../lib/start-limit-filter/filter-type.interface';
+import { StartLimitFilter } from '../../../lib/start-limit-filter/start-limit-filter.class';
+import { Material } from '../entities/material.entity';
 
 export class MaterialFilterQuery extends StartLimitFilter<Material> {
   @IsString()
   @IsOptional()
   name?: string;
 
-  @Transform(({ value }) => deserializeArray(String, `[${value}]`))
+  @Transform(({ value }) => typeof value === 'string' ? value.split(',') : undefined)
   @IsOptional()
   @IsString({ each: true })
   categories?: string[];
