91886d13c5ffd553eae7a1e5936913a05b62cbf9
Edmunds <folkmanis@gmail.com>
2024-10-09 06:59:20 +0000

    cleanup

--- src/entities/jobs/job-notify.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JobNotifyInterceptor } from './job-notify.interceptor.js';
-
-describe('JobNotifyInterceptor', () => {
-  it('should be defined', () => {
-    expect(new JobNotifyInterceptor()).toBeDefined();
-  });
-});
--- src/entities/jobs/update-job-files-location.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateJobFilesLocationInterceptor } from './update-job-files-location.interceptor.js';
-
-describe('UpdateJobFilesLocationInterceptor', () => {
-  it('should be defined', () => {
-    expect(new UpdateJobFilesLocationInterceptor()).toBeDefined();
-  });
-});
