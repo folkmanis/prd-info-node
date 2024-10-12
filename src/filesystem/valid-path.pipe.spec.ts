971409622ac7f457de98e91bf0eaf0c0a30b70df
Edmunds <folkmanis@gmail.com>
2024-10-12 14:32:00 +0000

    cleanup

--- src/filesystem/valid-path.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ValidPathPipe } from './valid-path.pipe.js';
-
-describe('ValidPathPipe', () => {
-  it('should be defined', () => {
-    expect(new ValidPathPipe()).toBeDefined();
-  });
-});
