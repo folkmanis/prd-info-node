10294b15a7df9fcfbe4bfac320296623fe458f81
Edmunds <folkmanis@gmail.com>
2026-02-25 09:15:40 +0200

    feat: add vehicleId property to RouteSheetFilterQuery and update toFilter method

--- src/entities/transportation/dto/route-sheet-filter.query.ts
+++ src/entities/transportation/dto/route-sheet-filter.query.ts
@@ -1,9 +1,10 @@
-import { Transform } from 'class-transformer';
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { IsNumber, IsObject, IsOptional, IsString } from 'class-validator';
 import { pickNotNull } from '../../../lib/pick-not-null.js';
 import { FilterType } from '../../../lib/start-limit-filter/filter-type.interface.js';
 import { StartLimitFilter } from '../../../lib/start-limit-filter/start-limit-filter.class.js';
 import { TransportationRouteSheet } from '../entities/route-sheet.entity.js';
+import { ObjectId, WithId } from 'mongodb';
 
 export class RouteSheetFilterQuery extends StartLimitFilter<TransportationRouteSheet> {
   @IsString()
@@ -27,7 +28,15 @@ export class RouteSheetFilterQuery extends StartLimitFilter<TransportationRouteS
   @IsNumber()
   month?: number;
 
-  toFilter(): FilterType<TransportationRouteSheet> {
+  @IsOptional()
+  @Type(() => ObjectId)
+  @Transform(({ value }) =>
+    typeof value === 'string' ? ObjectId.createFromHexString(value) : undefined,
+  )
+  @IsObject()
+  vehicleId?: ObjectId;
+
+  toFilter(): FilterType<WithId<TransportationRouteSheet>> {
     const { start, limit } = this;
     return {
       start,
@@ -39,6 +48,7 @@ export class RouteSheetFilterQuery extends StartLimitFilter<TransportationRouteS
           : undefined,
         year: this.year ? { $eq: this.year } : undefined,
         month: this.month ? { $eq: this.month } : undefined,
+        'vehicle._id': this.vehicleId,
       }),
     };
   }
--- src/version.ts
+++ src/version.ts
@@ -4,6 +4,6 @@ export interface Version {
 }
 
 export const VERSION: Version = {
-  apiBuild: 384,
-  appBuild: 384,
+  apiBuild: 385,
+  appBuild: 385,
 };
