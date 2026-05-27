5e4eed9ddb2a7f32fe02b12700a82949f4a162ad
Edmunds <folkmanis@gmail.com>
2026-05-27 15:01:12 +0300

    module and config update

--- @types/express-session/index.d.ts
+++ @types/express-session/index.d.ts
@@ -1,17 +1,14 @@
-import { User } from '../../src/entities/users/index.js';
+import { User } from '../../entities/users/index.ts';
 import { Credentials } from 'googleapis';
 
 declare module 'express-session' {
-
-    interface SessionData {
-        user: User;
-        lastSeen: {
-            ip: string,
-            date: Date,
-        };
-        redirectPath?: string;
-        tokens?: Credentials;
-    }
-
+  interface SessionData {
+    user: User;
+    lastSeen: {
+      ip: string;
+      date: Date;
+    };
+    redirectPath?: string;
+    tokens?: Credentials;
+  }
 }
-
--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,24 +1,28 @@
 import { OAuth2Client } from 'google-auth-library';
 import { gmail_v1 } from 'googleapis';
+import 'express';
 
-declare global {
+declare module 'express-serve-static-core' {
+  type Modules =
+    | 'kastes'
+    | 'system'
+    | 'jobs'
+    | 'paytraq'
+    | 'calculations'
+    | 'admin'
+    | 'xmf-search'
+    | 'xmf-upload'
+    | 'jobs-admin';
 
-    namespace Express {
+  interface Version {
+    apiBuild: number;
+    appBuild: number;
+  }
 
-        type Modules = 'kastes' | 'system' | 'jobs' | 'paytraq' | 'calculations' | 'admin' | 'xmf-search' | 'xmf-upload' | 'jobs-admin';
-
-        interface Request {
-            version?: Version;
-            instanceId?: string;
-            oAuth2?: OAuth2Client;
-            gmail?: gmail_v1.Gmail;
-        }
-
-    }
-
-
-    interface Version {
-        apiBuild: number;
-        appBuild: number;
-    }
+  interface Request {
+    version?: Version;
+    instanceId?: string;
+    oAuth2?: OAuth2Client;
+    gmail?: gmail_v1.Gmail;
+  }
 }
